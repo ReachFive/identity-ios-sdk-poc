6719a8b3c8e0261b7354fd92107680a31b13b234
egor <egor.neon@gmail.com>
2019-05-27 15:50:21 +0200

    wip

---
+++ IdentitySdkFacebook/IdentitySdkFacebook/IdentitySdkFacebook.h
@@ -0,0 +1,19 @@
+//
+//  IdentitySdkFacebook.h
+//  IdentitySdkFacebook
+//
+//  Created by Egor Berezovskiy on 27/05/2019.
+//  Copyright © 2019 Reachfive. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for IdentitySdkFacebook.
+FOUNDATION_EXPORT double IdentitySdkFacebookVersionNumber;
+
+//! Project version string for IdentitySdkFacebook.
+FOUNDATION_EXPORT const unsigned char IdentitySdkFacebookVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <IdentitySdkFacebook/PublicHeader.h>
+
+
