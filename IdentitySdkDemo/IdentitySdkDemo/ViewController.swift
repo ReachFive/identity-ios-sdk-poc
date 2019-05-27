2dc8948c61c17f9659ab68ff0d94c8025dd3a0be
egor <egor.neon@gmail.com>
2019-05-27 16:06:04 +0200

    wip

--- IdentitySdkDemo/IdentitySdkDemo/ViewController.swift
+++ IdentitySdkDemo/IdentitySdkDemo/ViewController.swift
@@ -5,15 +5,14 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        // Do any additional setup after loading the view.
+
         let provider = WebViewProvider()
         
         let sdkConfig = SdkConfig(domain: "local-sandbox.og4.me", clientId: "sg48CdAYohRPeRWZ9j1H")
         let reachfive = ReachFive(sdkConfig: sdkConfig, providersCreators: [provider])
         
         reachfive.show()
-
-        // Do any additional setup after loading the view.
     }
 
 }
--- IdentitySdkFacebook/IdentitySdkFacebook/IdentitySdkFacebook.h
+++ IdentitySdkFacebook/IdentitySdkFacebook/IdentitySdkFacebook.h
@@ -1,11 +1,3 @@
-//
-//  IdentitySdkFacebook.h
-//  IdentitySdkFacebook
-//
-//  Created by Egor Berezovskiy on 27/05/2019.
-//  Copyright © 2019 Reachfive. All rights reserved.
-//
-
 #import <UIKit/UIKit.h>
 
 //! Project version number for IdentitySdkFacebook.
@@ -15,5 +7,3 @@ FOUNDATION_EXPORT double IdentitySdkFacebookVersionNumber;
 FOUNDATION_EXPORT const unsigned char IdentitySdkFacebookVersionString[];
 
 // In this header, you should import all the public headers of your framework using statements like #import <IdentitySdkFacebook/PublicHeader.h>
-
-
