662e985546844ed28e3565fa36951f0f10a09516
egor <egor.neon@gmail.com>
2019-05-28 16:34:55 +0200

    wip

--- Sandbox/Sandbox/ViewController.swift
+++ Sandbox/Sandbox/ViewController.swift
@@ -1,21 +1,17 @@
 import UIKit
-//import IdentitySDKCore
-
-// pod 'IdentitySdkCore', :path => '../IdentitySdkCore'
+import IdentitySdkCore
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        /*
         let provider = WebViewProvider()
         
         let sdkConfig = SdkConfig(domain: "local-sandbox.og4.me", clientId: "sg48CdAYohRPeRWZ9j1H")
         let reachfive = ReachFive(sdkConfig: sdkConfig, providersCreators: [provider])
         
         reachfive.show()
-        */
     }
 
 }
