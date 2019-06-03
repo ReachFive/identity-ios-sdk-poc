0bb508dd98c246673e54009ebe608820355e6f29
egor <egor.neon@gmail.com>
2019-06-03 11:39:44 +0200

    wip

--- Sandbox/Pods/Target Support Files/IdentitySdkCore/IdentitySdkCore-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_IdentitySdkCore : NSObject
-@end
-@implementation PodsDummy_IdentitySdkCore
-@end
--- Sandbox/Pods/Target Support Files/IdentitySdkFacebook/IdentitySdkFacebook-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_IdentitySdkFacebook : NSObject
-@end
-@implementation PodsDummy_IdentitySdkFacebook
-@end
