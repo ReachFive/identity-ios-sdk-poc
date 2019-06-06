e8158e3e30a2dba0f987e69264eeaf379aa0da78
egor <egor.neon@gmail.com>
2019-06-06 15:52:40 +0200

    wip

--- Sandbox/Sandbox/ViewController.swift
+++ Sandbox/Sandbox/ViewController.swift
@@ -2,8 +2,18 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var emailInput: UITextField!
+    @IBOutlet weak var passwordInput: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
-
+    
+    @IBAction func login(_ sender: Any) {
+        let email = emailInput.text ?? ""
+        let password = passwordInput.text ?? ""
+        print("Login email=\(email) password=\(password)")
+        AppDelegate.shared().reachfive.loginWithPassword(username: email, password: password, success: { print($0) }, failure: { print($0) })
+    }
+    
 }
