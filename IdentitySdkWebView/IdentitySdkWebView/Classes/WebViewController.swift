2a6eca6beffa449c08d31194e13fefddcc19863d
egor <egor.neon@gmail.com>
2019-07-15 11:21:21 +0200

    wip

--- IdentitySdkWebView/IdentitySdkWebView/Classes/WebViewController.swift
+++ IdentitySdkWebView/IdentitySdkWebView/Classes/WebViewController.swift
@@ -4,19 +4,48 @@ import WebKit
 import IdentitySdkCore
 
 public class WebViewController: UIViewController, WKNavigationDelegate, WKUIDelegate {    
-    @IBOutlet weak var webView: WKWebView!
+    var webView: WKWebView!
+    
+    @IBOutlet var webViewContainer: UIView!
+    @IBOutlet weak var loader: UIActivityIndicatorView!
     
     var url: String? = nil
     var delegate: Callback<Dictionary<String, String?>, ReachFiveError>? = nil
     
     public override func viewDidLoad() {
         super.viewDidLoad()
+        /*
+        let configuration = WKWebViewConfiguration()
+        webView = WKWebView(frame: .zero, configuration: configuration)
+        webView.uiDelegate = self
+        view = webView
                 
         webView.allowsBackForwardNavigationGestures = true
         webView.navigationDelegate = self
+    
+         */
+        let configuration = WKWebViewConfiguration()
+        webView = WKWebView(frame: .zero, configuration: configuration)
+        webView.allowsBackForwardNavigationGestures = true
+        webView.navigationDelegate = self
+        webView.allowsLinkPreview = true
+        webView.uiDelegate = self
+        
+        webViewContainer.addSubview(webView)
+        webViewContainer.sendSubviewToBack(webView)
+        addConstraints(to: webView, with: webViewContainer)
         
         let url = URL(string: self.url!)!
         webView.load(URLRequest(url: url))
+    }
+    
+    func addConstraints(to webView: UIView, with superView: UIView) {
+        webView.translatesAutoresizingMaskIntoConstraints = false
+        let leadingConstraint = NSLayoutConstraint(item: webView, attribute: .leading, relatedBy: .equal, toItem: superView, attribute: .leading, multiplier: 1, constant: 0)
+        let trailingConstraint = NSLayoutConstraint(item: webView, attribute: .trailing, relatedBy: .equal, toItem: superView, attribute: .trailing, multiplier: 1, constant: 0)
+        let topConstraint = NSLayoutConstraint(item: webView, attribute: .top, relatedBy: .equal, toItem: superView, attribute: .top, multiplier: 1, constant: 0)
+        let bottomConstraint = NSLayoutConstraint(item: webView, attribute: .bottom, relatedBy: .equal, toItem: superView, attribute: .bottom, multiplier: 1, constant: 0)
+        superView.addConstraints([leadingConstraint, trailingConstraint, topConstraint, bottomConstraint])
     }
     
     func loadHtml(html: String) {
