c19446c2e6951232b9923249fd2c5e857199d4e2
egor <wadjetz@tuta.io>
2019-07-22 18:06:20 +0200

    wip

--- IdentitySdkCore/IdentitySdkCore/Classes/models/requests/ProfileRequest.swift
+++ IdentitySdkCore/IdentitySdkCore/Classes/models/requests/ProfileRequest.swift
@@ -2,99 +2,118 @@ import Foundation
 import ObjectMapper
 
 public class ProfileRequest: NSObject, ImmutableMappable {
-    public let email: String?
     public let password: String
+    public let email: String?
     public let phoneNumber: String?
-    public let gender: String?
-    public let name: String?
     public let givenName: String?
     public let middleName: String?
     public let familyName: String?
+    public let name: String?
     public let nickname: String?
-    public let username: String?
     public let birthdate: String?
+    public let profileURL: String?
     public let picture: String?
-    public let company: String?
+    public let username: String?
+    public let gender: String?
+    public let addresses: [Address]?
     public let locale: String?
-    public let address: AddressRequest?
-    public let customFields: Dictionary<String, Any>?
+    public let bio: String?
+    public let customFields: [String: Any]?
+    public let consents: [String: UserConsent]?
+    public let tosAcceptedAt: String?
+    public let liteOnly: Bool?
     
-    public init(
-        email: String?,
+    public required init(
         password: String,
+        email: String? = nil,
         phoneNumber: String? = nil,
-        gender: String? = nil,
-        name: String? = nil,
         givenName: String? = nil,
         middleName: String? = nil,
         familyName: String? = nil,
+        name: String? = nil,
         nickname: String? = nil,
-        username: String? = nil,
         birthdate: String? = nil,
+        profileURL: String? = nil,
         picture: String? = nil,
-        company: String? = nil,
+        username: String? = nil,
+        gender: String? = nil,
+        addresses: [Address]? = nil,
         locale: String? = nil,
-        address: AddressRequest? = nil,
-        customFields: Dictionary<String, Any>? = nil
+        bio: String? = nil,
+        customFields: [String: Any]? = nil,
+        consents: [String: UserConsent]? = nil,
+        tosAcceptedAt: String? = nil,
+        liteOnly: Bool? = nil
     ) {
-        self.email = email
         self.password = password
+        self.email = email
         self.phoneNumber = phoneNumber
-        self.gender = gender
-        self.name = name
         self.givenName = givenName
         self.middleName = middleName
         self.familyName = familyName
+        self.name = name
         self.nickname = nickname
-        self.username = username
         self.birthdate = birthdate
+        self.profileURL = profileURL
         self.picture = picture
-        self.company = company
+        self.username = username
+        self.gender = gender
+        self.addresses = addresses
         self.locale = locale
-        self.address = address
+        self.bio = bio
         self.customFields = customFields
+        self.consents = consents
+        self.tosAcceptedAt = tosAcceptedAt
+        self.liteOnly = liteOnly
     }
     
     public required init(map: Map) throws {
-        email = try? map.value("email")
         password = try map.value("password")
-        phoneNumber = try? map.value("phone_number")
-        gender = try? map.value("gender")
-        name = try? map.value("name")
         givenName = try? map.value("given_name")
         middleName = try? map.value("middle_name")
         familyName = try? map.value("family_name")
+        name = try? map.value("name")
         nickname = try? map.value("nickname")
-        username = try? map.value("username")
         birthdate = try? map.value("birthdate")
+        profileURL = try? map.value("profile_url")
         picture = try? map.value("picture")
-        company = try? map.value("company")
+        username = try? map.value("username")
+        gender = try? map.value("gender")
+        email = try? map.value("email")
+        phoneNumber = try? map.value("phone_number")
+        addresses = try? map.value("addresses")
         locale = try? map.value("locale")
-        address = try? map.value("address")
+        bio = try? map.value("bio")
         customFields = try? map.value("custom_fields")
+        consents = try? map.value("consents")
+        tosAcceptedAt = try? map.value("tos_accepted_at")
+        liteOnly = try? map.value("lite_only")
     }
     
     public func mapping(map: Map) {
-        email >>> map["email"]
         password >>> map["password"]
+        email >>> map["email"]
         phoneNumber >>> map["phone_number"]
-        gender >>> map["gender"]
-        name >>> map["name"]
         givenName >>> map["given_name"]
         middleName >>> map["middle_name"]
         familyName >>> map["family_name"]
+        name >>> map["name"]
         nickname >>> map["nickname"]
-        username >>> map["username"]
         birthdate >>> map["birthdate"]
+        profileURL >>> map["profile_url"]
         picture >>> map["picture"]
-        company >>> map["company"]
+        username >>> map["username"]
+        gender >>> map["gender"]
+        addresses >>> map["addresses"]
         locale >>> map["locale"]
-        address >>> map["address"]
+        bio >>> map["bio"]
         customFields >>> map["custom_fields"]
+        consents >>> map["consents"]
+        tosAcceptedAt >>> map["tos_accepted_at"]
+        liteOnly >>> map["lite_only"]
     }
     
     public override var description: String {
         return self.toJSONString(prettyPrint: true) ?? super.description
     }
 }
-
