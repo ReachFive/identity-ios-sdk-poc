9dd89ecdee29e525071c1fd1874e6ba0eeb107a0
François Devémy <francois.devemy@reach5.co>
2021-06-01 13:02:19 +0200

    Remove redundant return.

--- IdentitySdkGoogle/IdentitySdkGoogle/Classes/GoogleProvider.swift
+++ IdentitySdkGoogle/IdentitySdkGoogle/Classes/GoogleProvider.swift
@@ -6,11 +6,11 @@ import BrightFutures
 
 public class GoogleProvider: ProviderCreator {
     public static var NAME: String = "google"
-    
+
     public var name: String = NAME
-    
+
     public init() {}
-    
+
     public func create(
         sdkConfig: SdkConfig,
         providerConfig: ProviderConfig,
@@ -28,23 +28,23 @@ public class GoogleProvider: ProviderCreator {
 
 public class ConfiguredGoogleProvider: NSObject, Provider, GIDSignInDelegate {
     public var name: String = GoogleProvider.NAME
-    
+
     var sdkConfig: SdkConfig
     var providerConfig: ProviderConfig
     var reachFiveApi: ReachFiveApi
     var clientConfigResponse: ClientConfigResponse
-    
+
     var scope: [String]? = []
     var origin: String = ""
     var promise: Promise<AuthToken, ReachFiveError>?
-    
+
     public init(sdkConfig: SdkConfig, providerConfig: ProviderConfig, reachFiveApi: ReachFiveApi, clientConfigResponse: ClientConfigResponse) {
         self.sdkConfig = sdkConfig
         self.providerConfig = providerConfig
         self.reachFiveApi = reachFiveApi
         self.clientConfigResponse = clientConfigResponse
     }
-    
+
     public func sign(_ signIn: GIDSignIn!, didSignInFor user: GIDGoogleUser!, withError error: Error!) {
         if error != nil {
             self.promise?.failure(.AuthFailure(reason: error.localizedDescription))
@@ -69,7 +69,7 @@ public class ConfiguredGoogleProvider: NSObject, Provider, GIDSignInDelegate {
                 }
         }
     }
-    
+
     public func login(
         scope: [String]?,
         origin: String,
@@ -86,11 +86,11 @@ public class ConfiguredGoogleProvider: NSObject, Provider, GIDSignInDelegate {
         GIDSignIn.sharedInstance().signIn()
         return promise.future
     }
-    
+
     public func application(_ app: UIApplication, open url: URL, options: [UIApplication.OpenURLOptionsKey : Any]) -> Bool {
-        return GIDSignIn.sharedInstance().handle(url)
+        GIDSignIn.sharedInstance().handle(url)
     }
-    
+
     public func application(_ application: UIApplication, open url: URL, sourceApplication: String?, annotation: Any) -> Bool {
         var _: [String: AnyObject] = [
             UIApplication.OpenURLOptionsKey.sourceApplication.rawValue: sourceApplication as AnyObject,
@@ -98,14 +98,14 @@ public class ConfiguredGoogleProvider: NSObject, Provider, GIDSignInDelegate {
         ]
         return GIDSignIn.sharedInstance().handle(url)
     }
-    
+
     public func applicationDidBecomeActive(_ application: UIApplication) {}
-    
+
     public func logout() -> Future<(), ReachFiveError> {
         GIDSignIn.sharedInstance()?.signOut()
         return Future.init(value: ())
     }
-    
+
     public override var description: String {
         return "Provider: \(name)"
     }
